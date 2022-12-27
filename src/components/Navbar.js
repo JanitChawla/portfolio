92c218139e129830615ff35a9988c30631bab60a
Janit Chawla <68466788+JanitChawla@users.noreply.github.com>
2022-12-27 14:48:59 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ import AssignmentInd from "@material-ui/icons/AssignmentInd";
 
 const menuItems = [
   { listIcon: <Home />, listText: "Home", listPath: "/" },
-  { listIcon: <AssignmentInd />, listText: "Resume", listPath: "https://drive.google.com/file/d/1SjyCMyP4eNqMBaOdSbDfL0vVptXKjQOY/view" },
+  { listIcon: <AssignmentInd />, listText: "Resume", listPath: "https://drive.google.com/file/d/12hrjNVzJ9GlHxuMbVBP8o515wxKfc6G3/view?usp=sharing" },
   { listIcon: <Apps />, listText: "Projects", listPath: "/project" },
   { listIcon: <ContactMail />, listText: "Contact", listPath: "/contact" },
 //   { listIcon: <ContactMail />, listText: 'Skills', listPath: '/skills'}
